b3bbd26f6eee8884ed5b1db1ec37229bdc7af030
Bart van Stratum <bart@vanstratum.com>
2022-08-24 12:47:47 +0200

    Update canopy case and statistics script.

--- cases/canopy/statistics.py
+++ cases/canopy/statistics.py
@@ -13,7 +13,7 @@ def xr_read_all(f, groups=['default'], decode_times=True):
     return xr.merge(dss)
 
 
-s1 = xr_read_all('canopy.default.0000000.nc', decode_times=False)
+s1 = xr_read_all('canopy.default.0000000.nc', groups=['default', 'budget'], decode_times=False)
 s1m = s1.sel(time=slice(1500, 2400)).mean(dim='time')
 
 # Friction velocity at canopy top:
@@ -22,31 +22,52 @@ ustar_zc = ((s1m.u_flux[10]**2 + s1m.v_flux[10]**2))**(1/4)
 # Canopy height:
 zc = 10.
 
+# Mean scaled velocity profile
 pl.figure()
 pl.plot(s1m.u/ustar_zc, s1m.z/zc)
-pl.xlabel(r'$u/u*$ (-)')
+pl.xlabel(r'$u/u_*$ (-)')
 pl.ylabel(r'$z/z_c$ (-)')
 pl.xlim(0, 8)
 pl.ylim(0, 3)
 
+
+# Variance profiles
 pl.figure()
 pl.subplot(221)
 pl.plot(np.sqrt(s1m.u_2) / ustar_zc, s1m.z/zc)
-pl.xlabel(r'$\sigma_u/u*$ (-)')
+pl.xlabel(r'$\sigma_u/u_*$ (-)')
 pl.ylabel(r'$z/z_c$ (-)')
 pl.ylim(0, 6)
 pl.xlim(0, 2.5)
 
 pl.subplot(222)
 pl.plot(np.sqrt(s1m.v_2) / ustar_zc, s1m.z/zc)
-pl.xlabel(r'$\sigma_v/u*$ (-)')
+pl.xlabel(r'$\sigma_v/u_*$ (-)')
 pl.ylabel(r'$z/z_c$ (-)')
 pl.ylim(0, 6)
 pl.xlim(0, 2)
 
 pl.subplot(223)
 pl.plot(np.sqrt(s1m.w_2) / ustar_zc, s1m.zh/zc)
-pl.xlabel(r'$\sigma_w/u*$ (-)')
+pl.xlabel(r'$\sigma_w/u_*$ (-)')
 pl.ylabel(r'$z/z_c$ (-)')
 pl.ylim(0, 6)
 pl.xlim(0, 1.5)
+
+pl.subplot(224)
+pl.plot(s1m.ke / ustar_zc**2, s1m.z/zc)
+pl.xlabel(r'$KE/u_*^2$ (-)')
+pl.ylabel(r'$z/z_c$ (-)')
+pl.ylim(0, 6)
+pl.xlim(0, 50)
+
+# TKE budget
+pl.figure()
+pl.plot(s1m.tke_shear, s1m.z/zc, label='shear')
+pl.plot(s1m.tke_turb, s1m.z/zc, label='turb')
+pl.plot(s1m.tke_diff, s1m.z/zc, label='diff')
+pl.plot(s1m.tke_pres, s1m.z/zc, label='pres')
+pl.legend()
+pl.ylim(0,4)
+pl.ylabel(r'$z/z_c$ (-)')
+pl.xlabel(r'dTKE/dt (m$^2$ s$^{-3}$)')
